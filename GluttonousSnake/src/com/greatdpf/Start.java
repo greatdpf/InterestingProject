f17f9ec561e82dbf8523085ee31b0486a80402c2
dpf <greatdpf@163.com>
2020-11-01 23:25:58 +0800

    创建贪吃蛇项目

---
+++ GluttonousSnake/src/com/greatdpf/Start.java
@@ -0,0 +1,17 @@
+package com.greatdpf;
+
+/**
+ * 开始游戏
+ *
+ * @author : dpf
+ * @version : V1.0
+ * @date : 2020/11/1
+ */
+public class Start {
+    /**
+     * 程序的入口
+     */
+    public static void main(String[] args){
+
+    }
+}
